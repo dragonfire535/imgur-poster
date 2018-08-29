11806982afa7e22df87a10644bf509f6f5bd0595
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-08-29 10:47:58 +0000

    Bug Fixes

--- Poster.js
+++ Poster.js
@@ -1,13 +1,13 @@
 const { POSTER_ID, POSTER_TOKEN, POSTER_ALBUM_ID, POSTER_TIME, IMGUR_KEY } = process.env;
 const request = require('node-superfetch');
 const used = new Set();
-const time = Number.parseFloat(POSTER_TIME);
+const time = Number.parseFloat(POSTER_TIME) || 1.8e+6;
 let cache = null;
 
 setInterval(async () => {
 	try {
 		let images;
-		if (cache && used.size !== images.length) {
+		if (cache && used.size !== cache.length) {
 			images = cache;
 		} else {
 			const { body } = await request
